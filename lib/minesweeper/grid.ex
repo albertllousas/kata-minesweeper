c5b9a09135719b99af2816f55c62977d714871da
Albert Ortiz <aortiz@thoughtworks.com>
2018-03-17 00:21:39 +0100

    some refactor adding square to eval to all grid functions

--- lib/minesweeper/grid.ex
+++ lib/minesweeper/grid.ex
@@ -12,15 +12,20 @@ defmodule Minesweeper.Grid do
 
   @spec discover_hints(grid :: Grid.t) :: Grid.t
   def discover_hints(grid) do
-    do_discover_hints(grid)
+    do_discover_hints(grid, {0,0})
   end
 
-  defp do_discover_hints(%Grid{size: {0, 0}, squares: %{}}), do: %Grid{}
+  defp do_discover_hints(%Grid{ size: size} = grid, square_to_eval) when size == square_to_eval, do: grid
 
-  defp do_discover_hints( %Grid{size: {1, 1}, squares: %{{0,0}=>:mine}}),
+  defp do_discover_hints(%Grid{ squares: %{{0,0}=>:mine}} = grid, square_to_eval),
        do: %Grid{size: {1, 1}, squares: %{{0,0}=>:mine}}
 
-  defp do_discover_hints( %Grid{size: {1, 1}, squares: %{{0,0}=>:safe}}),
+  defp do_discover_hints(%Grid{ squares: %{{0,0}=>:safe}} = grid, square_to_eval),
        do: %Grid{size: {1, 1}, squares: %{{0,0}=>0}}
+
+
+  #  recursive until current evaluated position equals to size
+  #       public method calculate hint summing adjacents
+  # nextSquare(grid size, current square)
 
 end

43ade9c1ed5922af4a92a83dcd566fbd30402e4d
Albert Ortiz <aortiz@thoughtworks.com>
2018-03-17 09:45:57 +0100

    Adding testing for invalid squares in adjacent calculation

--- test/minesweeper/square_test.exs
+++ test/minesweeper/square_test.exs
@@ -3,9 +3,21 @@ defmodule Minesweeper.SquareTest do
 
   alias Minesweeper.Square
 
-  test "an empty grid should have an empty list of adjacents" do
+  test "an {0,0} grid should have an empty list of adjacents" do
     adjacents = Square.adjacents({0,0}, {0,0})
     assert adjacents === []
   end
 
+  test "in a {1,1} grid the unique posible square should have an empty list of adjacents" do
+    adjacents = Square.adjacents({1,1}, {0,0})
+    assert adjacents === []
+  end
+
+  test "an invalid square should have an empty list of adjacents" do
+    adjacents = Square.adjacents({1,1}, {100,-100})
+    assert adjacents === []
+  end
+
+
+#  not posible errors only negative grids
 end

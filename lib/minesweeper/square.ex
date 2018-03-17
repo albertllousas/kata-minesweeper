6ac0c4f3612b2fedb1a69175291573488d1bed6a
Albert Ortiz <aortiz@thoughtworks.com>
2018-03-17 23:36:52 +0100

    refactoring next square calculationa and adding missing tests

--- lib/minesweeper/square.ex
+++ lib/minesweeper/square.ex
@@ -4,23 +4,25 @@ defmodule Minesweeper.Square do
   @type size :: {non_neg_integer, non_neg_integer}
 
   @spec adjacents(grid_size :: size, square :: position) :: [position]
-  def adjacents(grid_size, square) do
-
-    {x, y} = square
+  def adjacents({size_x, size_y} = grid_size, {x, y} = square) do
 
     all_posible_neighbours =
-      [{x - 1, y - 1},{x, y - 1},{x + 1, y - 1}, {x - 1, y},{x + 1, y }, {x - 1, y + 1},{x, y + 1},{x + 1, y + 1}]
+      [{x - 1, y - 1}, {x, y - 1}, {x + 1, y - 1}, {x - 1, y}, {x + 1, y}, {x - 1, y + 1}, {x, y + 1}, {x + 1, y + 1}]
 
-    Enum.filter(all_posible_neighbours, fn(neighbour) -> elem(neighbour, 0) < elem(grid_size, 0)  end)
-    |> Enum.filter( fn(neighbour) -> elem(neighbour, 0) >= 0  end )
-    |> Enum.filter( fn(neighbour) -> elem(neighbour, 1) < elem(grid_size, 1)  end )
-    |> Enum.filter( &(elem(&1, 1) >= 0) )
+    Enum.filter(all_posible_neighbours, fn (neighbour) -> elem(neighbour, 0) < size_x  end)
+    |> Enum.filter(fn (neighbour) -> elem(neighbour, 0) >= 0  end)
+    |> Enum.filter(fn (neighbour) -> elem(neighbour, 1) < size_y  end)
+    |> Enum.filter(&(elem(&1, 1) >= 0))
 
   end
 
   @spec next(grid_size :: size, current_square :: position) :: {:next, position} | {:out_of_grid}
-  def next(grid_size, current_square) do
-    {:out_of_grid}
+  def next({size_x, size_y} = grid_size, {x, y} = square) do
+    cond do
+      x < size_x - 1 and y < size_y - 1 -> {:ok, {x + 1, y}}
+      y < size_y - 1 -> {:ok, {0, y + 1}}
+      true -> {:out_of_grid}
+    end
   end
 
 end
--- test/minesweeper/square_test.exs
+++ test/minesweeper/square_test.exs
@@ -18,38 +18,58 @@ defmodule Minesweeper.SquareTest do
     assert adjacents === []
   end
 
-  test "in an standard grid a square in the top left corner should have three adjacents" do
+  test "in a standard grid, a square in the top left corner should have three adjacents" do
     adjacents = Square.adjacents({3, 3}, {0, 0})
     assert adjacents === [{1, 0}, {0, 1}, {1, 1}]
   end
 
-  test "in an standard grid a square in the top right corner should have three adjacents" do
+  test "in a standard grid, a square in the top right corner should have three adjacents" do
     adjacents = Square.adjacents({3, 3}, {2, 0})
     assert adjacents === [{1, 0}, {1, 1}, {2, 1}]
   end
 
-  test "in an standard grid a square in the bottom left corner should have three adjacents" do
+  test "in a standard grid, a square in the bottom left corner should have three adjacents" do
     adjacents = Square.adjacents({4, 4}, {0, 3})
     assert adjacents === [{0, 2}, {1, 2}, {1, 3}]
   end
 
-  test "in an standard grid a square in the bottom right corner should have three adjacents" do
+  test "in a standard grid, a square in the bottom right corner should have three adjacents" do
     adjacents = Square.adjacents({5, 5}, {4, 4})
     assert adjacents === [{3, 3}, {4, 3}, {3, 4}]
   end
 
-  test "in an standard grid a square in the top-half boundary should have five adjacents" do
+  test "in a standard grid, a square in the top-half boundary should have five adjacents" do
     adjacents = Square.adjacents({5, 4}, {3, 0})
     assert adjacents === [{2, 0}, {4, 0}, {2, 1}, {3, 1}, {4, 1}]
   end
 
-  test "in an standard grid a square in middle should have eight adjacents" do
+  test "in a standard grid, a square in middle should have eight adjacents" do
     adjacents = Square.adjacents({10, 10}, {3, 3})
     assert adjacents === [{2, 2}, {3, 2}, {4, 2}, {2, 3}, {4, 3}, {2, 4}, {3, 4}, {4, 4}]
   end
 
   test "in a non size grid should not be a next square" do
-    next = Square.next({0,0}, {0,0})
+    next = Square.next({0, 0}, {0, 0})
+    assert next === {:out_of_grid}
+  end
+
+  test "in a standard grid, the next square of a square in the middle is the one at right" do
+    next = Square.next({5, 5}, {3, 3})
+    assert next === {:ok, {4, 3}}
+  end
+
+  test "in a standard grid, the next square of a square in the left boundary is the first on the row below" do
+    next = Square.next({5, 5}, {4, 3})
+    assert next === {:ok, {0, 4}}
+  end
+
+  test "in a standard grid, the next square of the square in the bottom right corner is out of the grid" do
+    next = Square.next({5, 5}, {4, 4})
+    assert next === {:out_of_grid}
+  end
+
+  test "in a standard grid, the next square of an invalid square will be out of the grid" do
+    next = Square.next({5, 5}, {6, 6})
     assert next === {:out_of_grid}
   end
 

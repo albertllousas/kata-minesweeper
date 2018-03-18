41c5dd62cdf41667dfec00001b7f971af6a4a5b7
Albert Ortiz <aortiz@thoughtworks.com>
2018-03-18 23:50:28 +0100

    adding implementation to write the output

--- lib/minesweeper/output/console_writer.ex
+++ lib/minesweeper/output/console_writer.ex
@@ -4,5 +4,21 @@ defmodule Minesweeper.Output.ConsoleWriter do
   alias Minesweeper.Grid
 
   @spec write_grids([Grid.t]) :: none
-  def write_grids(_), do: []
+  def write_grids(grids) do
+
+    for {grid, index} <- Enum.with_index(grids, 1) do
+
+      {rows, columns} = grid.size
+      IO.puts "Field ##{index}:"
+
+      for y <- 0 .. (rows - 1) do
+        row = for x <- 0 .. (columns - 1), do: grid.squares[{x,y}]
+        IO.puts Enum.join(row) |> String.replace( "mine", "*")
+      end
+
+      IO.puts ""
+    end
+
+  end
+
 end
--- test/minesweeper/output/console_writer_test.exs
+++ test/minesweeper/output/console_writer_test.exs
@@ -10,4 +10,16 @@ defmodule Minesweeper.Output.ConsoleWriterTest do
     assert capture_io(fn -> ConsoleWriter.write_grids([]) end) == ""
   end
 
+  test "should print in console output a single grid with for squares" do
+
+    grid = %Grid{
+      size: {2, 2},
+      squares: %{
+        {0, 0} => :mine, {1, 0} => 1,
+        {0, 1} => 1, {1, 1} => 1
+      }
+    }
+    assert capture_io(fn -> ConsoleWriter.write_grids([grid]) end) == ""
+  end
+
 end

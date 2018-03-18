c7ac0f858e431893d8b1bb00c5750ed5d5cc2fd3
Albert Ortiz <aortiz@thoughtworks.com>
2018-03-18 02:04:27 +0100

    refactoring calculation of a single hint method and adding missing tests

--- lib/minesweeper/grid.ex
+++ lib/minesweeper/grid.ex
@@ -1,14 +1,15 @@
 defmodule Minesweeper.Grid do
 
   @type hint :: 0..8
-  @type square :: (:mine | :safe | hint)
+  @type square_value :: (:mine | :safe | hint)
   @type position :: {non_neg_integer, non_neg_integer}
-  @type squares :: %{position => square}
+  @type squares :: %{position => square_value}
   @type t :: %Minesweeper.Grid{size: {non_neg_integer, non_neg_integer}, squares: squares}
 
   defstruct size: {0, 0}, squares: %{}
 
   alias Minesweeper.Grid
+  alias Minesweeper.Square
 
   @spec discover_hints(grid :: Grid.t) :: Grid.t
   def discover_hints(grid) do
@@ -23,8 +24,14 @@ defmodule Minesweeper.Grid do
   defp do_discover_hints(%Grid{ squares: %{{0,0}=>:safe}} = grid, square_to_eval),
        do: %Grid{size: {1, 1}, squares: %{{0,0}=>0}}
 
+  @spec hint(grid :: Grid.t, square :: position) :: hint
+  def hint(grid, square) do
+    Square.adjacents(grid.size, square)
+    |> Enum.map(fn adjacent -> grid.squares[adjacent] end)
+    |> Enum.filter(fn square_value -> square_value == :mine end)
+    |> Enum.count
+  end
 
-  def hint(grid, square), do: 0
 
   #  recursive until current evaluated position equals to size
   #       public method calculate hint summing adjacents Enum. 0 .. 8, quizas un reduce desde 0
--- test/minesweeper/grid_test.exs
+++ test/minesweeper/grid_test.exs
@@ -6,6 +6,56 @@ defmodule Mineswepper.GridTest do
   @empty_grid %Grid{}
 
 
+  test "should hint with 'zero' when a square has no adjacents mines" do
+    safe_grid = %Grid{
+      size: {1, 1},
+      squares: %{
+        {0, 0} => :safe, {1, 0} => :safe,
+        {0, 1} => :safe, {1, 1} => :safe
+      }
+    }
+    hint = Grid.hint safe_grid, {0, 0}
+    assert hint === 0
+  end
+
+  test "should hint with 'one' when a square has only one adjacent mine" do
+    grid = %Grid{
+      size: {2, 2},
+      squares: %{
+        {0, 0} => :safe, {1, 0} => :mine,
+        {0, 1} => :safe, {1, 1} => :safe
+      }
+    }
+    hint = Grid.hint grid, {0, 0}
+    assert hint === 1
+  end
+
+
+  test "should hint with 'three' when a square has three adjacent mines" do
+    grid = %Grid{
+      size: {2, 2},
+      squares: %{
+        {0, 0} => :safe, {1, 0} => :mine,
+        {0, 1} => :mine, {1, 1} => :mine
+      }
+    }
+    hint = Grid.hint grid, {0, 0}
+    assert hint === 3
+  end
+
+  test "should hint with 'eight' when a square is totally sorrounded by mines" do
+    grid = %Grid{
+      size: {3, 3},
+      squares: %{
+        {0, 0} => :mine, {1, 0} => :mine, {2, 0} => :mine,
+        {0, 1} => :mine, {1, 1} => :safe, {2, 1} => :mine,
+        {0, 2} => :mine, {1, 2} => :mine, {2, 2} => :mine
+      }
+    }
+    hint = Grid.hint grid, {1, 1}
+    assert hint === 8
+  end
+
   test "in an empty grid should have empty discovered hints" do
     hints = Grid.discover_hints(@empty_grid)
     assert hints === @empty_grid
@@ -52,19 +102,7 @@ defmodule Mineswepper.GridTest do
   #   assert hints === expected
   #  end
 
-  test "should hint as 'zero' when a square has no adjacents mines" do
-    safe_grid = %Grid{
-      size: {1, 1},
-      squares: %{
-        {0, 0} => :safe,
-        {1, 0} => :safe,
-        {0, 1} => :safe,
-        {1, 1} => :safe
-      }
-    }
-    hint = Grid.hint safe_grid, {0, 0}
-    assert hint === 0
-  end
+
 
 
 end
